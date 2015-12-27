84560acfc2726119c7b334bf51b87593fb2238ad
pepe navarro <oscar.novillo@gmail.com>
2015-12-27 12:57:44 +0100

    conf

---
+++ NodejsWebApp1/server.js
@@ -0,0 +1,8 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.write("hola otra vez");
+	res.end('Hello World\n');
+	
+}).listen(port);
